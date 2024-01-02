5c911cc6b26fdd1da98e912c04befc1ee95334a1
ViktorGirda <viktorgirda@yandex.ru>
2024-01-02 08:21:10 +0300

    Разбил проект на страницы. Настроил роуты

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,11 @@
 import { useEffect } from "react";
 import { useSelector } from "react-redux";
 
+import './app.css';
+
 import { dataSelector, fetchDataItems } from "./Redux/dataSlice";
 import {useAppDispatch} from "./Redux/Store";
+import RouteComponent from "./Components/RouteComponent/RouteComponent";
 
 function App() {
   const { items } = useSelector(dataSelector);
@@ -17,7 +20,9 @@ function App() {
 
   console.log(items);
   return (
-    <div className="App">1</div>
+    <div className="wrapper">
+      <RouteComponent />
+    </div>
   );
 }
 
